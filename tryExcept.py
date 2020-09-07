8bd52b567ba0d8b5ea35e596ed36941394dd45ff
gege <gegepl123@gmail.com>
2020-09-07 09:47:48 +0200

    Dodałem notatki z raise i assert i samodzielne wyrzucanie wyjątków

--- tryExcept.py
+++ tryExcept.py
@@ -17,7 +17,7 @@ try:
 except (ZeroDivisionError, TypeError):  #obsługiwanie kilku wyjątków jednocześnie
     print("Dodawanie roznych typow zmiennych i dzielenie przez zero")
 
-#IndexError - wyjątek gdy wybieramy nieistniejący ineks tablicy
+#IndexError - wyjątek gdy wybieramy nieistniejący indeks tablicy
 tab = []
 try:
     print(tab[3])
@@ -25,3 +25,16 @@ except: #wykona się po wychwyceniu dowolnego błędu w try
     print("cos sie popsulo")
 finally: #nie jest wymagany
     print("wykonam się niezależnie czy wystąpił błąd")
+
+
+def dzielenie(x, y):
+    assert not y == 0, "Opis błędu"  #wyrzuca error jeśli nie zostało spełnione
+    if y == 0:
+        raise TypeError("Treść wyjątku jaką chcemy wyświetlić przy wykonywaniu programu z błędem")
+        #sam wyrzucę błąd, niezależnie czy zgadza się z tym który by wystąpił
+    print(x / y)
+try:
+    dzielenie(2, 0)
+except TypeError: #błąd taki jaki podaliśmy w rise przy definiowaniu funkcji
+    print("podano zero do funkcji") #wykonanie except przy błędzie w funkcji
+    raise #wyrzucenie błędu pomimo except
