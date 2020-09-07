79fa88825fee13ca18d6b7f881e96ca85fc8b233
gege <gegepl123@gmail.com>
2020-09-07 17:03:55 +0200

    Działania na plikach + notatki

---
+++ files/files.py
@@ -0,0 +1,10 @@
+plik = open("tekst.txt", "a") #"w" - writeable "r" - readable "a" - appendable
+if plik.writable():
+    plik.write(input("Wprowadź tekst: "))# + "\n" - tworzy pustą linię pod napisem
+plik.close()
+
+plik = open("tekst.txt", "r")
+if plik.readable():
+    for x in plik: #read() - wypisuje treść pliku readlines() - wypisuje treść pliku jako listę
+        print(x)
+plik.close() #zaaaaaawsze pamiętaj
