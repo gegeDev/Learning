f2201116895ed41e840939046405853fa4bb5ecc
gege <gegepl123@gmail.com>
2020-09-07 17:27:20 +0200

    poprawka w files.py dodawanie entera na końcu

--- files/files.py
+++ files/files.py
@@ -1,6 +1,6 @@
 plik = open("tekst.txt", "a") #"w" - writeable "r" - readable "a" - appendable
 if plik.writable():
-    plik.write(input("Wprowadź tekst: "))# + "\n" - tworzy pustą linię pod napisem
+    plik.write(input("Wprowadź tekst: " + "\n"))# + "\n" - tworzy pustą linię pod napisem
 plik.close()
 
 plik = open("tekst.txt", "r")
