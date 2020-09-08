0eec0794f846e6ab01fa914bb0bbb2ffa3993970
gege <gegepl123@gmail.com>
2020-09-08 16:08:33 +0200

    Dodałem jedną opcję w comprehensions

--- comprehensions.py
+++ comprehensions.py
@@ -4,6 +4,8 @@ nowa = [i * 2 for i in lista] #mnożenie wszystkich elementów listy razy dwa i 
 print(lista)
 print(nowa)
 nowa2 = [i + 2 for i in lista if i % 2 == 0] #do pętli w deklaracji można dopisać instrukcje warunkowe, elementy które nie spełniają instrukcji warunkowej nie zostaną wpisane do listy
+lista = [i for i in range(10, 21, 2)] #tak też można, to wszystkie liczby parzyste od 10 do 20
+print(lista)
 print(nowa2)
 
 #FORMATOWANIE STRINGÓW
